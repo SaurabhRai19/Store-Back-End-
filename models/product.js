a2f0b0cfdd4fe8c84e387404ae98b53d9b495291
Saurabh Rai <42387065+SaurabhRai19@users.noreply.github.com>
2020-12-18 15:51:06 +0530

    Initial Commit

---
+++ models/product.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 2000
+    },
+    price: {
+      type: Number,
+      required: true,
+      maxlength: 32,
+      trim: true
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+      required: true
+    },
+    stock: {
+      type: String
+    },
+    casts:{
+      type: String,
+      required: true
+    },
+    sold: {
+      type: Number,
+      default: 0
+    },
+    video: {
+      type: String,
+      required: true
+    },
+    director: {
+      type: String,
+      required: true
+    },
+    photo: {
+      data: Buffer,
+      contentType: String
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
